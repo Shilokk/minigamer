3ab2f47d71e355fe60cbd23c390d2e4af2de9fb7
Shilo K <65488745+Shilokk@users.noreply.github.com>
2021-05-25 23:13:44 -0700

    commmit

--- schemas/db.js
+++
@@ -1,8 +0,0 @@
-const { MongoClient } = require("salvage.db");
-const db = new MongoClient({
-  mongoURI: require("../data/config.json").mongo,
-  schema: {
-    name: "minigamer",
-  },
-});
-module.exports = db;
